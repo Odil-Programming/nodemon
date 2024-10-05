f92f046a575cfacee48d1ba6dc7b011eb7307be8
Odil-Programming <odilchikfurqatovich@gmail.com>
2024-10-05 19:35:30 +0500

    done

---
+++ config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        // Подключаемся к MongoDB без устаревших опций
+        await mongoose.connect(`mongodb+srv://odilchikfurqatovich:odil2901-odil@cluster0.f34dj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
